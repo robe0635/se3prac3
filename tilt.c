1fdd6c0f37e5503aeea2273c57d42a65ebffd172
Jayden Roberts <robe0635@flinders.edu.au>
2014-07-03 16:17:01 +0930

    Added sliding function to tilt.
    testing to see if there is any errors after adding the sliding function.

--- tilt.c
+++ tilt.c
@@ -7,7 +7,19 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-
+    int a = 0;
+    for (a = 2; a > 0 a--) 
+    {
+      int b = 0;
+      for(b = 3; b >= 1; b--) 
+      {
+        if (line [b] != 0 && line [b-1] == 0) 
+        {
+          line [b-1] = line[b];
+          line [b] = 0;
+        }
+      }
+    }
   // combine tiles as required
 
   return 0;
