a9b8c0962ce82a1a5f2d20cb280f2a910977a84f
Jayden Roberts <robe0635@flinders.edu.au>
2014-07-03 17:19:55 +0930

    moved methods from top to bottom
    moved the slide method to where the combine method is, and moved the combine method to where the slide method was.

--- tilt.c
+++ tilt.c
@@ -6,20 +6,21 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  // slide tiles to the left
-  int a = 0; 
-  for (a = 3; a >= 1; a --) {
-    if (line[a] != 0 && line [a - 1] == 0) {
-      line[a - 1] = line[a];
-      line[a]= 0;
-    }
-  }
   // combine tiles as required
   int b = 0;
   for (b = 3; b >= 1; b --) {
     if (line[b] == line[b + 1]) {
       line[b] = line[b] * 2;
       line[b + 1] = 0;
+    }
+  }
+  
+  // slide tiles to the left
+  int a = 0; 
+  for (a = 3; a >= 1; a --) {
+    if (line[a] != 0 && line [a - 1] == 0) {
+      line[a - 1] = line[a];
+      line[a]= 0;
     }
   }
 
