ca154c6961b23d83691eb2f49eca18227e002d0b
Jayden Roberts <robe0635@flinders.edu.au>
2014-07-03 17:24:19 +0930

    further fix syntax errors in both functions.

--- tilt.c
+++ tilt.c
@@ -6,9 +6,9 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  // combine tiles as required
+  // combinng tiles as required
   int b = 0;
-  for (b = 3; b >= 1; b --) {
+  for (b = 0; b >= 2; b ++) {
     if (line[b] == line[b + 1]) {
       line[b] = line[b] * 2;
       line[b + 1] = 0;
